e68187171c64b768660bd32e84439eaf0d77362b
Amy van Duin <30554@ma-web.nl>
2020-09-14 10:48:09 +0200

    kaasblokjes

---
+++ Variabelen.py
@@ -0,0 +1,17 @@
+# handig dit commentaar
+# string
+Naam = "Amy" 	#dit is string
+aanUit  True  	#dit is een boolean
+puntGetal = 6.9 #dit is een float of double
+karakter = "#" 	
+heelGetal = 123	#dit is integer
+
+puntGetal *= 2
+
+print (naam)
+print (aanUit)
+print (puntGetal)
+
+
+
+
---
+++ voorbeeld python.py
@@ -0,0 +1,10 @@
+# Doel: Voor en achternaam kunnen invoeren. De computer weergeeft mijn volledige naam.
+# input()
+# variabelen
+# print()
+
+print("Wat is jouw naam?")
+naam = input()
+print (naam)
+
+print("")
