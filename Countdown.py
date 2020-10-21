90a66a5582941e47fa480cd57b3dd44827486373
Amy van Duin <30554@ma-web.nl>
2020-10-21 13:57:36 +0200

    amy

---
+++ Countdown.py
@@ -0,0 +1,7 @@
+import time
+
+for i in range(1000, 0, -1):
+    print(i)
+    time.sleep(0.011)
+
+print("Blast off!!")
---
+++ dailychioces.py
@@ -0,0 +1,75 @@
+print("Vandaag Gaan we 5 dagelijkse keuzes krijgen die iedereen dagelijks voor hun krijgen.")
+print("Daar gaan we dan....")
+print = input
+
+input("de eerste senario: je wakker staat om 07:45 wat doen je? \n A. langer in je bed blijven liggen en minder tijd hebben om om te kleden. \n B. meteen opstaan en goed wakker worden.")
+antwoord = input("Typ hier uw keuze >> ")
+if antwoord == "A" or antwoord == "a":
+	print(" je hebt gekozen voor antwoord a")
+	print("je blijft liever in je bed liggen to het laatste moment.")
+	print("")
+elif antwoord == "B" or antwoord == "b":
+	print("je hebt gekozen voor antwoord b")
+	print("je staat meteen goed op en begint de dag goed ")
+	print("")
+else:
+	print("")
+
+
+input("Doe je liever een hoodie aan of een trui \n A. Hoodie \n B. een trui")
+antwoord = input("Typ hier uw keuze >>")
+if antwoord == "A" or antwoord == "a":
+	print("je hebt gekozen voor antwoord a")
+	print("je draagt liever een hoodie")
+	print("")
+elif antwoord == "B" or antwoord == "b":
+	print("je hebt gekozen voor antwoord b")
+	print("je draagt dus liever een trui")
+	print("")
+else:
+	print("")
+
+
+input("kies je voor het openbaar vervoer of pak je liever de fiets of ander voertuig \n A. ik kies voor openbaar vervoer \n B. ik kies voor de fiets of een ander voertuig.")
+antwoord = input("Typ hier uw keuze >>")
+if antwoord == "A" or antwoord == "a":
+	print("je hebt gekozen voor antwoord a")
+	print(" je pakt dus liever het openbaarvervoer ")
+	print("")
+elif antwoord == "B" or antwoord == "b":
+	print("je hebt gekozen voor antwoord b")
+	print(" je pakt dus liever je fiets of een ander soort voertuig")
+	print("")
+else:
+	print("")
+
+
+input(" eet je liever een broodje ei of een broodje hagelslag \n A.broodje ei \nB. broodje hagelslag")
+antwoord = input("Typ hier uw keuze >>")
+if antwoord == "A" or antwoord == "a":
+	print("je hebt gekozen voor antwoord a")
+	print(" je eet liever ene broodje ei ")
+	print("")
+elif antwoord == "B" or antwoord == "b":
+	print("je hebt gekozen voor antwoord b")
+	print(" je eet liever een broodje hagelslag")
+	print("")
+else:
+	print("")
+
+input("Je ziet iemand die je kent in de bus zitten. \n A. ga je hoi zeggen \n B. of laat je die persoon met rust ")
+antwoord = input("Typ hier uw keuze >>")
+if antwoord == "A" or antwoord == "a":
+	print("je hebt gekozen voor antwoord a")
+	print("je gaat dus de persoon te begroeten ")
+	print("")
+elif antwoord == "B" or antwoord == "b":
+	print("je hebt gekozen voor antwoord b")
+	print(" je laat die persoon met rust")
+	print("")
+else:
+	print("")
+	
+	
+
+
