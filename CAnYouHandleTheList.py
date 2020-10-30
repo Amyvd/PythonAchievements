b06c3ebce2d9f1d492c1e6eaa714aca606bb52e7
Amy van Duin <30554@ma-web.nl>
2020-10-30 11:27:57 +0100

    derde

---
+++ CAnYouHandleTheList.py
@@ -0,0 +1,8 @@
+lijstje1 = [1, True, "goodbye", 3566, "twintig"]
+print(lijstje1)
+lijstje1.append("Hanwe")
+print(lijstje1)
+lijstje1.remove("goodbye")
+print(lijstje1)
+lijstje1.insert(2, 69)
+print(lijstje1)
